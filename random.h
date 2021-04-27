cf1ef08691d50330645604ce02c514c7dac48eea
Aayush-patel-1696 <69187017+Aayush-patel-1696@users.noreply.github.com>
2021-04-27 12:03:01 +0530

    Delete random.h

--- random.h
+++
@@ -1,9 +0,0 @@
-#include <random>
-
-std::default_random_engine generator;
-std::uniform_real_distribution<double> distribution(0,1);
-#pragma omp declare simd
-double randk()
-{
-    return distribution(generator);
-}
